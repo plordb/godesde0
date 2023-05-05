2dcc7ff5c7ede859812f2358f7ddd180d84b65d0
Pablo <pablo@debian.MiRed>
2023-05-05 15:50:32 -0600

    Siguiente Commit

--- arreglos_slices/arreglos.go
+++ arreglos_slices/arreglos.go
@@ -10,5 +10,12 @@ func MuestroArreglos() {
 	tabla[7] = 33
 	tabla[2] = 54
 
+	tabla2 := [10]string{"Pablo", "Juan"}
+
 	fmt.Println(tabla)
+	fmt.Println(tabla2)
+
+	for i := 0; i < len(tabla); i++ {
+		fmt.Println(tabla[i])
+	}
 }
